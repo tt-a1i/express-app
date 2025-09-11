a9ec46b7aa01b575d041c71b3fb62fdd996ae946
tt-a1i <tt-a1i@users.noreply.github.com>
2025-09-11 09:37:39 +0800

    style: 将404处理函数改为箭头函数
    将404错误处理中间件从函数声明改为箭头函数语法，保持代码风格一致。

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/users', usersRouter);
 app.use('/protected', protectedRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
